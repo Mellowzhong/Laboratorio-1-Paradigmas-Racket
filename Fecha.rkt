282a83f28b39f1c8bf38cf1640cc4848b351fcc6
Francisco Sanchez <francisco.sanchez.g@usach.cl>
2023-03-30 23:29:27 -0300

    Se agrega la funcion login y logout

--- Fecha.rkt
+++ Fecha.rkt
@@ -2,8 +2,10 @@
 (provide (all-defined-out))
 (require racket/date)
 
+;Identifica la fecha actual
 (define date-now (current-date))
 
+;Lleva la fecha actual a una lista
 (define list-date
     (list
       (date-year date-now)
@@ -11,7 +13,8 @@
       (date-day date-now)
       )
   )
-  
+
+;Transforma la lista de la fecha a un string
 (define fecha (string-append (number->string(list-ref list-date 0)) "/"
                             (number->string(list-ref list-date 1)) "/"
                             (number->string(list-ref list-date 2))
--- TDA_User.rkt
+++ TDA_User.rkt
@@ -1,6 +1,7 @@
 #lang racket
 (provide (all-defined-out))
 
+;Identifica si es que un elemento esta dentro de una lista
 (define (filter-element element lst)
   (cond [(null? lst) #f]
         [(equal? element (car lst)) #t]
@@ -8,16 +9,28 @@
     )
 )
 
+;Uno dos distas simulando el append
 (define (unir list-1 list-2)
-    (if (null? list-1);si la primera lista esta vacia retorno el segundo 
+    (if (null? list-1);si la primera lista esta vacia retorna el segundo 
         (list list-2)
-        (cons (car list-1) (unir (cdr list-1) list-2)));si la primera lista no es vacia, hago car y cdrs para concatenar por partes
+        (cons (car list-1) (unir (cdr list-1) list-2)));si la primera lista no es vacia, se hace car y cdrs para concatenar por partes
     )
 
+;Agrega usuarios al sistema
 (define (add-user sys  user)
     (if (filter-element user (list-ref sys 0)) sys
          (cons (unir (list-ref sys 0) user) (cdr sys))
         )
 )
 
+;Agrega un true al lado de los user para identificar que estan logeados
+(define (login sys user) 
+    (if (filter-element user (list-ref sys 0)) (cons (unir (list-ref sys 0) #t) (cdr sys))
+        (sys)
+        )
+    )
 
+;Quita los true para que se sepa que no esta logeado
+(define (logout sys)
+    (cons (filter string? (list-ref sys 0)) (cdr sys))
+    )
